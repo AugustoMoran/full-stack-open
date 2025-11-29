f282a505ee6254aadbfc5da9fac69b8a7e81c76e
AugustoMoran <augusto.moran.informatica@gmail.com>
2025-11-29 17:33:23 -0300

    separar logica en funciones

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -2,25 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  const increasiByOne =  () =>{
+      handleClick()
+      setCounter(counter + 1)  
+      }
+  const setToZero = () => setCounter(0)
 
-
-  const handleClick = () => {
-    console.log('clicked')
-  }
+  const handleClick = () => console.log('clicked')
 
   return (
     <div>
       <div>{counter}</div>
 
-      <button onClick={ () =>{
-      handleClick()
-      setCounter(counter + 1)
-      }   
-      }>
+      <button onClick={increasiByOne}>
         plus
       </button>
-      
-      <button onClick={() => setCounter(0)}>
+
+      <button onClick={setToZero}>
         zero
       </button>
     </div>
