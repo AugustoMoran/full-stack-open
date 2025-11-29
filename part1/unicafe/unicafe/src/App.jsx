63a82d91b553cf6614bd3b406587e12e51df9faa
AugustoMoran <augusto.moran.informatica@gmail.com>
2025-11-29 18:27:54 -0300

    pasando el estado a componentes hijos

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,26 +1,42 @@
 import { useState } from 'react'
-
+const Display = ({counter}) =>{
+  return(
+  <div>{counter}</div>
+)
+}
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+
+  const Buton = ({click, Text}) =>{
+    return(
+      <button onClick={click}> {Text}</button>
+    )
+  }
+  const handleClick = () => console.log('clicked')
+
   const increasiByOne =  () =>{
       handleClick()
       setCounter(counter + 1)  
-      }
+      
+  }
   const setToZero = () => setCounter(0)
+  
+  const descreasiByOne = () => {setCounter(counter - 1)}
 
-  const handleClick = () => console.log('clicked')
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter = {counter} />
 
-      <button onClick={increasiByOne}>
-        plus
-      </button>
-
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Buton
+       click={increasiByOne}
+       Text={"plus"} />
+      <Buton
+       click={setToZero}
+       Text={"zero"} />
+      <Buton
+       click={descreasiByOne}
+       Text={"restar"} />
     </div>
   )
 }
