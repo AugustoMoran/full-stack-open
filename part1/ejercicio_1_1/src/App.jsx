1edb3cc2542d86c1e9593e5489df2dae22829e99
AugustoMoran <augusto.moran.informatica@gmail.com>
2025-11-29 12:32:12 -0300

    1.5 se reorganiza el codigo para que todos los datos del curso esten dentro de un solo objeto course

--- part1/ejercicio_1_1/src/App.jsx
+++ part1/ejercicio_1_1/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 const Header = (props) => {
   console.log(props)
   return(
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 
 }
@@ -29,8 +29,9 @@ const Total = (props) => {
   )
 }
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts =[ {
+  const course ={
+    name: 'Half Stack application development',
+    parts : [{
     name :'Fundamentals of React',
     exercises : 10
   },
@@ -42,11 +43,12 @@ const App = () => {
     name : 'State of a component',
     exercises : 14
   }]
+  }
   return (
     <div>
       <Header course = {course} />
-      <Content parts = {parts} />
-      <Total parts = {parts} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
 
     </div>
   )
