ae3c6594089ce12cc50cad7f3cbd50c7f2586a91
AugustoMoran <augusto.moran.informatica@gmail.com>
2025-11-29 18:50:42 -0300

    refactorizacion de los componentes

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,42 +1,37 @@
 import { useState } from 'react'
-const Display = ({counter}) =>{
-  return(
-  <div>{counter}</div>
-)
-}
+const Display = ({counter}) => <div>{counter}</div>
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const Buton = ({click, Text}) =>{
-    return(
-      <button onClick={click}> {Text}</button>
-    )
-  }
+  const Buton = ({onClick, Text}) => <button onClick={onClick}> {Text}</button>
+  
   const handleClick = () => console.log('clicked')
 
   const increasiByOne =  () =>{
-      handleClick()
-      setCounter(counter + 1)  
+    console.log('increasing, value before', counter)
+    handleClick()
+    setCounter(counter + 1)  
       
   }
-  const setToZero = () => setCounter(0)
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
   
-  const descreasiByOne = () => {setCounter(counter - 1)}
+  const descreasiByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)}
 
 
   return (
     <div>
       <Display counter = {counter} />
 
-      <Buton
-       click={increasiByOne}
-       Text={"plus"} />
-      <Buton
-       click={setToZero}
-       Text={"zero"} />
-      <Buton
-       click={descreasiByOne}
-       Text={"restar"} />
+      <Buton onClick={increasiByOne} Text={"plus"} />
+      <Buton onClick={setToZero} Text={"zero"} />
+      <Buton onClick={descreasiByOne} Text={"restar"} />
     </div>
   )
 }
