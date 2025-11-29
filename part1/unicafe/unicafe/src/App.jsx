52e4b7b720f951ec0619f5ddadf0e32fa1c2c373
AugustoMoran <augusto.moran.informatica@gmail.com>
2025-11-29 17:12:31 -0300

    control de eventos hacemus un boton que suma uno cada vez que se da click uso de onclick y uo que lo vuelve a cero

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
 
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
 
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  console.log('rendering ...' , counter)
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+
+      <button onClick={ () =>{
+      handleClick()
+      setCounter(counter + 1)
+      }   
+      }>
+        plus
+      </button>
+      
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
   )
 }
 
